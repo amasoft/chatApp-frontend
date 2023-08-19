52f6011f0c0f59d1b5f660fc631a4e11d67041b1
amasoft <amadifaraday@gmail.com>
2023-08-19 13:42:19 +0100

    fix lottie issue on vercel

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 // import axios from "axios";
-import axios from "axios";
 import { ChatState } from "../Context/ChatProvider";
 import SideBar from "../Miselinouss/SideBar";
 import { Box } from "@chakra-ui/layout";
